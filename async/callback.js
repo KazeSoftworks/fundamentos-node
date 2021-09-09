2973ec36bc9977c1e1e09f8600eb346a858aef86
kazesoftworks <kazesoftworks@gmail.com>
2021-09-08 22:57:02 -0500

    Explicacion sobre callbacks

---
+++ async/callback.js
@@ -0,0 +1,21 @@
+const hola = (nombre, miCallback) => {
+  setTimeout(() => {
+    console.log("Hola " + nombre);
+    miCallback();
+  }, 1500);
+};
+
+const adios = (nombre, otroCallback) => {
+  setTimeout(() => {
+    console.log("Adios, " + nombre);
+    otroCallback();
+  }, 1000);
+};
+
+console.log("Iniciando proceso");
+
+hola("Pedro", () => {
+  adios("Pedro", () => {
+    console.log("TErminando el proceso");
+  });
+});
