11459a5d473236e3a9b39b60497ee2cf5b2be974
kazesoftworks <kazesoftworks@gmail.com>
2021-09-11 01:09:00 -0500

    Explicacion de importacion de modulos en commonJS y ES6

---
+++ modulos/paquetes/modulo/index.js
@@ -0,0 +1,9 @@
+//Traer nuestro modulo
+//import modulo from "./modulo.js";
+const modulo = require("./modulo.js");
+
+//Ejecutar una funcion del modulo
+console.log(modulo);
+//modulo();
+console.log(modulo.prop1);
+modulo.saludar();
---
+++ modulos/paquetes/modulo/modulo.js
@@ -0,0 +1,6 @@
+const saludar = () => {
+    console.log("Hola mundo");
+};
+
+//export default modulo;
+module.exports = { saludar, prop1: "Hola soy un prop" };
