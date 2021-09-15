b93ab230eb8dcbcaa807ce77375f492ca263fe77
Spellkaze <spellkaze@live.com>
2021-09-14 22:06:07 -0500

    Cambios para probar debugger propio de node con --inspect

--- modulos/http.js
+++ modulos/http.js
@@ -1,30 +1,37 @@
-const http = require("http");
+const http = require('http');
 
 const router = (request, response) => {
-    console.log("Nueva peticion");
-    console.log(request.url);
+	console.log('Nueva peticion');
+	console.log(request.url);
 
-    switch (request.url) {
-        case "/hola":
-            response.writeHead(201, { "Content-Type": "text/plain" });
-            response.write("Hola que tal");
-            response.end();
-            break;
-        default:
-            response.writeHead(404, { "Content-Type": "text/plain" });
-            response.write("Error 404");
-            response.end();
-    }
+	switch (request.url) {
+		case '/hola':
+			let saludo = hola();
+			response.writeHead(201, { 'Content-Type': 'text/plain' });
+			response.write(saludo);
+			response.end();
+			break;
+		default:
+			response.writeHead(404, { 'Content-Type': 'text/plain' });
+			response.write('Error 404');
+			response.end();
+	}
 
-    //Escribir respuesta al usuario
-    // response.writeHead(201, { "Content-Type": "text/plain" });
-    // response.write("HTTP en NodeJS");
+	//Escribir respuesta al usuario
+	// response.writeHead(201, { "Content-Type": "text/plain" });
+	// response.write("HTTP en NodeJS");
 
-    response.end();
+	response.end();
 };
 
-http.createServer((request, response) => {
-    router(request, response);
-}).listen(3000);
+http
+	.createServer((request, response) => {
+		router(request, response);
+	})
+	.listen(3000);
 
-console.log("Escuchando http en el puerto 3000");
+function hola() {
+	return 'Hola que tal';
+}
+
+console.log('Escuchando http en el puerto 3000');
